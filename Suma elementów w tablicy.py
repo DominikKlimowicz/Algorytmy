cb077b86cb703ddb3585ab5a08ce25843dc105e2
DominikKlimowicz <115640983+DominikKlimowicz@users.noreply.github.com>
2023-04-24 18:52:42 +0200

    LAB 4

--- Suma elementów w tablicy.py
+++ Suma elementów w tablicy.py
@@ -1,9 +1,20 @@
-def sum_dziel_i_zwyciezaj(arr, l, r):
-    if l == r:
-        return arr[l]
+Tablica = [1, 2, 3, 4, 5, 6]
+n = len(Tablica)
+wynik = 0
+c=0
+zmieniona = Tablica
+TZ=0
 
-    mid = (l + r) // 2
-    left_sum = sum_dziel_i_zwyciezaj(arr, l, mid)
-    right_sum = sum_dziel_i_zwyciezaj(arr, mid + 1, r)
-
-    return left_sum + right_sum
+while c <= len(Tablica):
+    n=len(Tablica)-c
+    zmieniona = Tablica[TZ:]
+    while n > 0:
+        if n == 1:
+            wynik += zmieniona[0]
+            break
+        mid = n // 2
+        n = mid
+        zmieniona = zmieniona[:mid]
+    c+=1
+    TZ+=1
+print(wynik)
